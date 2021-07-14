2af44630f456ad8459dc4b4cb18012372a90b430
mflem8 <matthewfleming8@gmail.com>
2021-07-14 14:13:03 -0400

    commit tree 1a384e8a409252693907272db70fbb586422c8b4

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+const negativizeArr = [1, 2, 3, -9];
+const nameArr = ['paul', 'gurney', 'vladimir', 'jessica', 'chani'];
+
+// Map Functions 
+
+const mapToNegativize = () => {
+    const negativizedArr = negativizeArr.map(x => x *= -1);
+    return negativizedArr;
+}
+
+const mapToNoChange = () => {
+    const originalNames = nameArr.map(x => x);
+    return originalNames;
+}
+
+const mapToDouble = () => {
+    const doubledArr = negativizeArr.map(x => x * 2);
+    return doubledArr;
+}
+
+const mapToSquare = () => {
+    const squaredArr = negativizeArr.map(x => x * x);
+    return squaredArr;
+}
+
+// Reduce Functions
+const sourceArray = [1, 2, 3];
+
+function reduceToTotal() {
+    return sourceArray.reduce((accumulator, currentValue) => accumulator + currentValue);
+
+};
+
+reduceToTotal(100);
+
